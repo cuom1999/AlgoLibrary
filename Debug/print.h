0a26d7b0cf4eaa7f3c9a5e7f24ebc42730d33c28
Phuoc Dinh Le <47278241+cuom1999@users.noreply.github.com>
2021-07-04 15:25:16 -0500

    Create print.h

---
+++ Debug/print.h
@@ -0,0 +1,45 @@
+template<typename T, typename V>
+ostream& operator<< (ostream &out, const pair<T, V> &x) {
+    out << '(' << x.first << ", " << x.second << ')';
+    return out;
+}
+
+template<typename T, size_t SIZE>
+ostream& operator<< (ostream &out, array<T, SIZE> &v){
+    int f = 0; 
+    for (auto &i: v) {
+        out << (f++ ? ", " : "[") << i; 
+    }
+    out << "]";
+    return out;
+}
+
+template<typename T>
+ostream& operator<< (ostream &out, vector<T> &v){
+    int f = 0; 
+    for (auto &i: v) {
+        out << (f++ ? ", " : "[") << i; 
+    }
+    out << "]";
+    return out;
+}
+
+template<typename T>
+ostream& operator<< (ostream &out, set<T> &v){
+    int f = 0; 
+    for (auto &i: v) {
+        out << (f++ ? ", " : "{") << i; 
+    }
+    out << "}";
+    return out;
+}
+
+template<typename T, typename V>
+ostream& operator<< (ostream &out, map<T, V> &v){
+    int f = 0; 
+    for (auto &i: v) {
+        out << (f++ ? ", " : "{") << i; 
+    }
+    out << "}";
+    return out;
+}
