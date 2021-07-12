e371f53db4ac5d0a3b8be50bfc83e14c6ff2cef8
Phuoc Dinh Le <47278241+cuom1999@users.noreply.github.com>
2021-07-12 01:28:33 -0500

    Update Index_Tree.h

--- DataStructure/Index_Tree.h
+++ DataStructure/Index_Tree.h
@@ -5,6 +5,6 @@ using namespace __gnu_pbds;
 typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> IndexTree;
 
 // Ex;
-s={2,5,6,10}
-*s.find_by_order(2)=6;
+IndexTree s = {2, 5, 6, 10};
+*s.find_by_order(2) == 6;
 s.order_of_key(x) == lower_bound(x)-s.begin();
