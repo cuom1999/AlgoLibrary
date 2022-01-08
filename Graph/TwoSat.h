b84e8770113f1c33593742a12556048b9a786f7f
Phuoc Dinh Le <47278241+cuom1999@users.noreply.github.com>
2022-01-08 21:02:49 +0700

    Update TwoSat.h

--- Graph/TwoSat.h
+++ Graph/TwoSat.h
@@ -1,71 +1,144 @@
-// Usage: init(n)  : maximum number of vertices
-//        add(p, q): add a 2-SAT p | q
-//        solve()  : return 1 if assignable
-// Note: we use -i = ~i
+// init(n): maximum number of vertices
+// solve(): return 1 if assignable
+// -i = ~i
+ 
+struct TwoSat {
+    int n;
+    int cnt = 0;
+    vector<vector<int>> a; 
+    vector<int> color, num, low; // color 1=False, 2=True
+    stack<int> st;
+    bool invalid = false;
+ 
+    bool minimize(int &a, int b) {
+        if (a > b) a = b; 
+        else return false; 
+        return true;
+    }
+ 
+    TwoSat(int n): n(n) {
+        a.resize(2 * n + 5);
+        color.resize(2 * n + 5);
+        num.resize(2 * n + 5);
+        low.resize(2 * n + 5);
+    }
+ 
+    int getColor(int u) {
+        return color[u + n];
+    }
+ 
+    void setColor(int u, int x){
+        if (color[u + n] == (x^3)) invalid = true; else color[u + n] = x;
+        if (color[-u + n] == x) invalid = true; else color[-u + n] = (x^3);
+    }
+ 
+    void tarjan(int u){
+        num[u] = low[u] = ++cnt; st.push(u);
+ 
+        for (auto v: a[u]) {
+            if (num[v]) minimize(low[u], num[v]);
+            else tarjan(v), minimize(low[u], low[v]);
+            if (color[v] == 1) setColor(u - n, 1); // False
+        }
+ 
+        if (low[u] == num[u]){
+            int v = 0 + n;
+            if (color[u] == 0) setColor(u - n, 2); // True
+            do {
+                v = st.top(); st.pop();
+                setColor(v - n, color[u]);
+                num[v] = low[v] = 0x33334444;
+            } while (u != v);
+        }
+    }
+ 
+    void add(int p, int q) {
+        a[-p + n].push_back(q + n);
+        a[-q + n].push_back(p + n);
+    }
+ 
+    bool solve() {
+        for (int i = 1; i <= n; i++) {
+            a[i + n].push_back(0 + n);
+            a[-i + n].push_back(0 + n);
+        }
+ 
+        for (int i = 1; i <= n; i++) {
+            if (!num[i + n]) tarjan(i + n);
+            if (!num[-i + n]) tarjan(-i + n);
+        }
+        return !invalid;
+    }
+};
+RAW Paste Data
+// init(n): maximum number of vertices
+// solve(): return 1 if assignable
+// -i = ~i
 
 struct TwoSat {
-    int numVer;
-    int cnt;
-    vector<int> * a; //
-    int *color, *num, *low; // color 1=False, 2=True
+    int n;
+    int cnt = 0;
+    vector<vector<int>> a; 
+    vector<int> color, num, low; // color 1=False, 2=True
     stack<int> st;
     bool invalid = false;
 
-    #define SetLength2(a, n, t) a=((t*) calloc(n, sizeof(t))) + (n)/2
-
     bool minimize(int &a, int b) {
-        if (a>b) a = b; 
+        if (a > b) a = b; 
         else return false; 
         return true;
     }
 
-    TwoSat(int n) {
-        numVer = n;
-        SetLength2(a, n*2+5, vector<int>);
-        SetLength2(color, n*2+5, int);
-        SetLength2(num, n*2+5, int);
-        SetLength2(low, n*2+5, int);
+    TwoSat(int n): n(n) {
+        a.resize(2 * n + 5);
+        color.resize(2 * n + 5);
+        num.resize(2 * n + 5);
+        low.resize(2 * n + 5);
     }
 
-    void set_color(int u, int x){
-        if (color[u]==(x^3)) invalid=true; else color[u]=x;
-        if (color[-u]==x) invalid=true; else color[-u]=(x^3);
+    int getColor(int u) {
+        return color[u + n];
     }
 
-    void tarzan(int u){
-        num[u]=low[u]=++cnt; st.push(u);
+    void setColor(int u, int x){
+        if (color[u + n] == (x^3)) invalid = true; else color[u + n] = x;
+        if (color[-u + n] == x) invalid = true; else color[-u + n] = (x^3);
+    }
 
-        for (int i=0,v; v=a[u][i]; i++){
+    void tarjan(int u){
+        num[u] = low[u] = ++cnt; st.push(u);
+
+        for (auto v: a[u]) {
             if (num[v]) minimize(low[u], num[v]);
-            else tarzan(v), minimize(low[u], low[v]);
-            if (color[v]==1) set_color(u, 1); // False
+            else tarjan(v), minimize(low[u], low[v]);
+            if (color[v] == 1) setColor(u - n, 1); // False
         }
-        if (low[u]==num[u]){
-            int v=0;
-            if (color[u]==0) set_color(u, 2); // True
+
+        if (low[u] == num[u]){
+            int v = 0 + n;
+            if (color[u] == 0) setColor(u - n, 2); // True
             do {
-                v=st.top(); st.pop();
-                set_color(v, color[u]);
-                num[v]=low[v]=0x33334444;
-            } while (u!=v);
+                v = st.top(); st.pop();
+                setColor(v - n, color[u]);
+                num[v] = low[v] = 0x33334444;
+            } while (u != v);
         }
     }
-
 
     void add(int p, int q) {
-        a[-p].push_back(q);
-        a[-q].push_back(p);
+        a[-p + n].push_back(q + n);
+        a[-q + n].push_back(p + n);
     }
 
     bool solve() {
-        for (int i = 1; i <= numVer; i++) {
-            a[i].push_back(0);
-            a[-i].push_back(0);
+        for (int i = 1; i <= n; i++) {
+            a[i + n].push_back(0 + n);
+            a[-i + n].push_back(0 + n);
         }
 
-        for (int i = 1; i <= numVer; i++) {
-            if (!num[i]) tarzan(i);
-            if (!num[-i]) tarzan(-i);
+        for (int i = 1; i <= n; i++) {
+            if (!num[i + n]) tarjan(i + n);
+            if (!num[-i + n]) tarjan(-i + n);
         }
         return !invalid;
     }
