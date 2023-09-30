10d227f30d5eedbf1d5e8d37d4a5d2d02df20b49
Phuoc Dinh Le <47278241+cuom1999@users.noreply.github.com>
2023-09-30 15:26:46 -0500

    Update DoubleHash.h

--- String/DoubleHash.h
+++ String/DoubleHash.h
@@ -1,11 +1,13 @@
 // Constructor: HashModule(s), O(len(s))
 // Usage      : query(l, r) - get hash(s[l..r]), O(1)
+template<long long base=3599, long long mod=(long long)1e9 + 7>
 struct HashModule {
-    long long mod;
     vector<long long> pw, hash;
-    HashModule(string s = "", long long base = 4599, long long p = 1e9 + 7) {
-        pw[0] = 1;
+    HashModule(string s="") {
         int n = s.size();
+        pw.resize(n + 1);
+        hash.resize(n + 1);
+        pw[0] = 1;
         for (int i = 1; i <= n; i++) {
             pw[i] = pw[i - 1] * base % mod;
         }
@@ -22,12 +24,13 @@ struct HashModule {
 };
 
 struct DoubleHash{
-    HashModule *hm1, *hm2;
-    DoubleHash(string s, long long base1 = 451999, long long mod1 = 1e9 + 9, long long base2 = 4599, long long mod2 = 1e9 + 7) {
-        hm1 = new HashModule(s, base1, mod1);
-        hm2 = new HashModule(s, base2, mod2);
+    HashModule<312321312, (long long)1e9 + 7> hm1;
+    HashModule<123123123, (long long)1e9 + 9> hm2;
+    DoubleHash(string s) {
+        hm1 = HashModule<312321312, (long long)1e9 + 7>(s);
+        hm2 = HashModule<123123123, (long long)1e9 + 9>(s);
     }
-    pair<int, int> query(long long l, long long r){
-        return pair<int, int>(hm1->query(l, r), hm2->query(l, r));
+    array<long long, 2> query(long long l, long long r){
+        return array<long long, 2>({hm1.query(l, r), hm2.query(l, r)});
     }
 };
